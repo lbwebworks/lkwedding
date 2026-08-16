f1aed4e6525f4abb3a760f2d6164ff2b8eb9dea1
Lee <lee.budaden@gmail.com>
2026-08-16 16:09:53 +0800

    Fixed icons when in github

--- src/App.tsx
+++ src/App.tsx
@@ -557,7 +557,7 @@ function App() {
               <div>
                 <h3>
                   <svg className="timeline-icon" aria-hidden="true">
-                    <use href={`/icons.svg#${item.icon}`} />
+                    <use href={`${import.meta.env.BASE_URL}icons.svg#${item.icon}`} />
                   </svg>
                   {item.title}
                 </h3>
@@ -730,7 +730,7 @@ function App() {
           <details key={item.question}>
             <summary>
               <svg className="faq-icon" aria-hidden="true">
-                <use href={`/icons.svg#${item.icon}`} />
+                <use href={`${import.meta.env.BASE_URL}icons.svg#${item.icon}`} />
               </svg>
               {item.question}
             </summary>
